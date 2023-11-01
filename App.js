c03b3631649e5a528f7eac3a6848c756e7920acc
Indah Putri Rahmi Nurilah <131329672+Indahprn@users.noreply.github.com>
2023-11-01 14:08:33 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nama: Indah </Text>
+      <Text>Kelas: XI-PPLG-3</Text>
+      <Text>Alamat: Jl.Khoer Affandi </Text>
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
